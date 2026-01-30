96a30e7943b8c0d325565d16f6946ab2dd62d5a1
Edcel Jeremy Macawili <edcel.macawili@dottystylecreative.com>
2026-01-30 10:54:46 +0800

    add pages

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
-const express = require('express');
-require('dotenv').config();
+const express = require("express");
+require("dotenv").config();
 
 const app = express();
 
 // Configure Pug as view engine
-app.set('view engine', 'pug');
+app.set("view engine", "pug");
 
 // Serve static files from /public
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + "/public"));
 
 // Basic GET route
-app.get('/', (req, res) => {
-    res.send('Server running');
+app.get("/", (req, res) => {
+	res.send("Server running");
 });
 
 // Listen on port 3000
-app.listen(3000, () => console.log('Listening on http://localhost:3000'));
+app.listen(3000, () => console.log("Listening on http://localhost:3000"));
