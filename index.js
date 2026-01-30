c5759d17e0050f252312d86c875879393e67a25e
Edcel Jeremy Macawili <edcel.macawili@dottystylecreative.com>
2026-01-30 10:58:00 +0800

    integrate hubspot api

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const express = require("express");
+const axios = require("axios");
 require("dotenv").config();
 
 const app = express();
+
+// Environment variables
+const HUBSPOT_ACCESS_TOKEN = process.env.HUBSPOT_ACCESS_TOKEN;
+const CUSTOM_OBJECT_NAME = process.env.CUSTOM_OBJECT_NAME;
 
 // Configure Pug as view engine
 app.set("view engine", "pug");
@@ -9,9 +14,26 @@ app.set("view engine", "pug");
 // Serve static files from /public
 app.use(express.static(__dirname + "/public"));
 
-// Basic GET route
-app.get("/", (req, res) => {
-	res.send("Server running");
+// GET route for homepage - fetch custom objects
+app.get("/", async (req, res) => {
+	const apiUrl = `https://api.hubspot.com/crm/v3/objects/${CUSTOM_OBJECT_NAME}?properties=name,description,price`;
+	const headers = {
+		Authorization: `Bearer ${HUBSPOT_ACCESS_TOKEN}`,
+		"Content-Type": "application/json",
+	};
+
+	try {
+		const response = await axios.get(apiUrl, { headers });
+		const records = response.data.results.map((record) => ({
+			name: record.properties.name,
+			description: record.properties.description,
+			price: record.properties.price,
+		}));
+		res.render("homepage", { title: "Custom Object Table", records });
+	} catch (error) {
+		console.error("Error fetching custom objects:", error.message);
+		res.status(500).send("Error fetching data from HubSpot");
+	}
 });
 
 // GET route for update custom object form
