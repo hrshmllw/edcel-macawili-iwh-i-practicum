c1eb98c10de9cc0a652100394fac8619793933a8
Edcel Jeremy Macawili <edcel.macawili@dottystylecreative.com>
2026-01-30 10:55:45 +0800

    add get route

--- index.js
+++ index.js
@@ -14,5 +14,12 @@ app.get("/", (req, res) => {
 	res.send("Server running");
 });
 
+// GET route for update custom object form
+app.get("/update-cobj", (req, res) => {
+	res.render("updates", {
+		title: "Update Custom Object Form | Integrating With HubSpot I Practicum",
+	});
+});
+
 // Listen on port 3000
 app.listen(3000, () => console.log("Listening on http://localhost:3000"));
